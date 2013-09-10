b33dc11377aa5a21bc8945b891cbd29525cd3f59
Hernan Meydac Jean <hernan.meydac@southworks.net>
2013-09-10 01:36:47 -0300

    First commit

---
+++ Dev/Hubs.RSS.Tests/RssReaderTests.cs
@@ -0,0 +1,34 @@
+﻿namespace Hubs.RSS.Tests
+{
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    [TestClass]
+    public class RssReaderTests
+    {
+        [TestMethod]
+        public void GivenRssUrlShouldPingFeedAndReturnTrue()
+        {
+            var reader = new RssReader();
+            var pingResult = reader.Ping(@"http://blog.nuget.org/feeds/atom.xml");
+            Assert.IsTrue(pingResult);
+            pingResult = reader.Ping(@"http://doesnotexist.com/");
+            Assert.IsFalse(pingResult);
+        }
+
+        [TestMethod]
+        public void GivenAnUrlShouldReadLastEntry()
+        {
+            var reader = new RssReader();
+            var entry = reader.GetLastEntry(@"http://blog.nuget.org/feeds/atom.xml");
+            Assert.IsFalse(string.IsNullOrEmpty(entry));
+        }
+
+        [TestMethod]
+        public void GivenNuGetFeedShouldReturn40Entries()
+        {
+            var reader = new RssReader();
+            var result = reader.GetFeedEntryCount(@"http://blog.nuget.org/feeds/atom.xml");
+            Assert.AreEqual(40, result);
+        }
+    }
+}
---
+++ Dev/Hubs.RSS/RssReader.cs
@@ -0,0 +1,31 @@
+﻿namespace Hubs.RSS
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    using QDFeedParser;
+
+    public class RssReader
+    {
+        public string GetLastEntry(string url)
+        {
+            var parser = new HttpFeedFactory();
+            var feed = parser.CreateFeed(new Uri(url));
+            return feed.Items.Last().Content;
+        }
+
+        public bool Ping(string url)
+        {
+            var parser = new HttpFeedFactory();
+            return parser.PingFeed(new Uri(url));
+        }
+
+        public int GetFeedEntryCount(string url)
+        {
+            var parser = new HttpFeedFactory();
+            var feed = parser.CreateFeed(new Uri(url));
+            return feed.Items.Count;
+        }
+    }
+}
